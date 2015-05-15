5684825d7ed0ac1d3c976414eed38e4f1e7675e6
SergioNoyano7 <Sergio>
2015-05-15 11:48:32 +0200

    Hola

---
+++ PracticaWebJSF-war/src/java/pwjsf/beans/PostearBean.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pwjsf.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import pwjsf.entity.Tpost;
+import pwjsf.entity.Tusuario;
+
+/**
+ *
+ * @author Sergio
+ */
+@ManagedBean
+@RequestScoped
+public class PostearBean {
+
+    @ManagedProperty(value="#{loginBean}")
+    private LoginBean loginBean;
+    
+    private String post;
+    
+    /**
+     * Creates a new instance of PostearBean
+     */
+    public PostearBean() {
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }
+    
+    public String doCrearPost(){
+        Tpost p = new Tpost();
+        p.setTexto(post);
+        //p.setTusuarioIdUser(user);
+        p.setTusuarioIdUser(null);
+        
+        return "preguntaImagen";
+    }
+}
