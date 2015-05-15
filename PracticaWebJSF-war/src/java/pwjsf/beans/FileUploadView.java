364729b1913ddf73c5dc7800145885dded3d5375
SergioNoyano7 <Sergio>
2015-05-15 14:59:21 +0200

    Postear Texto hecho(falta meter en base de datos)

---
+++ PracticaWebJSF-war/src/java/pwjsf/beans/FileUploadView.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pwjsf.beans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+ 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+/**
+ *
+ * @author Sergio
+ */
+
+@ManagedBean
+public class FileUploadView {
+     
+    private UploadedFile file;
+ 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+     
+    public String upload() {
+        String retorno;
+        if(file != null) {
+            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            //falta actualizar el post e insertarlo en la base de datos
+            retorno = "postear";
+        }else{
+            retorno = "errorImagen";
+        }
+        
+        return retorno;
+    }
+}
